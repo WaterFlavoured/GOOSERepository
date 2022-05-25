f992660c26a1bd70a531287df9c27c1e47085c1e
GitHub <michaelz9478@gmail.com>
2022-05-24 21:45:21 -0400

    1st commit

---
+++ TutorialPointJava/src/basicSyntax/MyFirstJavaProgram.java
@@ -0,0 +1,19 @@
+package basicSyntax;
+
+public class MyFirstJavaProgram {
+
+	   /* This is my first java program.
+	    * This will print 'Hello World' as the output
+	    */
+
+	   public static void main(String []args) {
+	     // for(int i = 0; i < 5; i++) {
+		   
+		  int[] numbers = {0,1,2,3,4};
+		  
+		  for(int i:numbers) {
+	    	  System.out.println("Hello World" + " " + i);
+	    	  System.out.println("HEHEHEHE");
+	      }
+	   }
+}
