36970962f58949b9672957e09e1da923dc71eb86
GitHub <michaelz9478@gmail.com>
2022-05-24 21:49:15 -0400

    For developer branch

--- TutorialPointJava/src/basicSyntax/MyFirstJavaProgram.java
+++ TutorialPointJava/src/basicSyntax/MyFirstJavaProgram.java
@@ -14,6 +14,7 @@ public class MyFirstJavaProgram {
 		  for(int i:numbers) {
 	    	  System.out.println("Hello World" + " " + i);
 	    	  System.out.println("HEHEHEHE");
+	    	  System.out.println("EHEIOHEOHE");
 	      }
 	   }
 }
